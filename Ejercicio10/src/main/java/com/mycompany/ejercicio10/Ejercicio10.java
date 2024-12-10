f06cfca2b99cce1fd14ed83e7eaa16377072f26d
ASUS <ASUS@DESKTOP-JFP8UPV>
2024-12-10 12:19:00 -0500

    Ejercicio 7

---
+++ Ejercicio10/src/main/java/com/mycompany/ejercicio10/Ejercicio10.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio10;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        
+        double valor_matricula;
+
+        int numero_inscripcion = 123456789;
+        String nombre_estudiante = "Jorge Borgues";
+        double patrimonio = 2500000;
+        int estrato_social = 5;
+        double valor_matricula_fijo = 50000;
+
+
+        if(patrimonio>2000000 && estrato_social>3){
+            valor_matricula = valor_matricula_fijo + ((patrimonio * 3 )/100);  
+            System.out.println("El número de numero_inscripción es:"+ numero_inscripcion); 
+            System.out.println("Nombres del estudiante :"+ nombre_estudiante); 
+            System.out.println("El valor de la matrícula es :"+ valor_matricula); 
+        } else {
+            valor_matricula = valor_matricula_fijo;
+            System.out.println("El número de numero_inscripción es:"+ numero_inscripcion); 
+            System.out.println("Nombres del estudiante :"+ nombre_estudiante); 
+            System.out.println("El valor de la matrícula es :"+ valor_matricula);
+        }
+
+        
+    }
+
+}
